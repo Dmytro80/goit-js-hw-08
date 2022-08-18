0c25ea189f69917f12283b05106aacd2f6ce8378
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-18 16:05:47 +0200

    Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,41 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createListItemsGallery(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryRef.addEventListener('click', onGalleryContainerClick);
+
+function makeItemGallery({ preview, original, description }) {
+  return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+}
+
+function createListItemsGallery(options) {
+  return options.map(makeItemGallery).join('');
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
