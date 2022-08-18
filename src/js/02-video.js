4966f3a4ece76167c607fbef673c32016242b402
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-18 18:57:11 +0200

    Task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeupdateOfStorage, 1000));
+
+const stopTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(stopTime);
+
+function onTimeupdateOfStorage(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
