4b430d41fee44dedd080654251f25e1986e2b886
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-20 13:57:00 +0200

    add two solutions Task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -36,3 +36,88 @@ function formSubmitHandler(evt) {
   console.log(JSON.parse(localStorage.getItem(FORM_STORAGE_KEY)));
   localStorage.removeItem(FORM_STORAGE_KEY);
 }
+
+// --------------Другий варіант реалізації задачі-----------------
+
+// const form = document.querySelector('.feedback-form');
+
+// let formData = {};
+
+// initForm();
+
+// form.addEventListener('input', throttle(onFormInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+//   const submitFormData = new FormData(form);
+
+//   let feedbackForm = {};
+
+//   submitFormData.forEach((value, name) => {
+//     feedbackForm[name] = value;
+//   });
+//   console.log(feedbackForm);
+
+//   evt.currentTarget.reset();
+
+//   localStorage.removeItem(FORM_STORAGE_KEY);
+// }
+
+// function onFormInput(evt) {
+//   formData[evt.target.name] = evt.target.value;
+//   localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function initForm() {
+//   const savedFormValue = localStorage.getItem(FORM_STORAGE_KEY);
+//   if (savedFormValue) {
+//     parseSavedFormValue = JSON.parse(savedFormValue);
+//     Object.entries(parseSavedFormValue).forEach(([name, value]) => {
+//       form.elements[name].value = value;
+//       formData[name] = value;
+//     });
+//   }
+// }
+
+// --------------------Третій варіант вирішення задачі---------------------
+
+// const form = document.querySelector('.feedback-form');
+
+// initForm();
+
+// form.addEventListener('input', throttle(onFormInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+//   const submitFormData = new FormData(form);
+
+//   let feedbackForm = {};
+
+//   submitFormData.forEach((value, name) => {
+//     feedbackForm[name] = value;
+//   });
+//   console.log(feedbackForm);
+
+//   evt.currentTarget.reset();
+
+//   localStorage.removeItem(FORM_STORAGE_KEY);
+// }
+
+// function onFormInput(evt) {
+//   let savedFormValue = localStorage.getItem(FORM_STORAGE_KEY);
+//   savedFormValue = savedFormValue ? JSON.parse(savedFormValue) : {};
+//   savedFormValue[evt.target.name] = evt.target.value;
+//   localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(savedFormValue));
+// }
+
+// function initForm() {
+//   let savedFormValue = localStorage.getItem(FORM_STORAGE_KEY);
+//   if (savedFormValue) {
+//     parseSavedFormValue = JSON.parse(savedFormValue);
+//     Object.entries(parseSavedFormValue).forEach(([name, value]) => {
+//       form.elements[name].value = value;
+//     });
+//   }
+// }
